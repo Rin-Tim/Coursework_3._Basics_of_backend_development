1d7f98fd3cc7e3b3ae212715b1b6ffc441e77a05
Rinat Timoshenko <rintim21v@gmail.com>
2024-04-11 11:37:13 +0300

    correcting data in files

--- backend/utils.py
+++ backend/utils.py
@@ -9,29 +9,36 @@ def open_json_file(file_path):
         return data
 
 
-def last_executed_five(operation_data):
+def last_executed_five():
     """Сортирует и выводит последние 5 операций на экран"""
+    file_path = '../data/operations.json'  # Путь к файлу json
+    operation_data = open_json_file(file_path)
+
     list_executed = []
 
+    # Фильтрация по статусу 'EXECUTED'
     for e in operation_data:
         if e.get('state') == 'EXECUTED':
             list_executed.append(e)
 
+    # Сортировка и вывод последних 5 операций
     last_list = sorted(list_executed, key=lambda x: x['date'], reverse=True)
+    five_op = last_list[:5]
 
-    return last_list
+    return five_op
 
 
 def data_fix(date_):
-    """"""
+    """Дата перевода представлена в формате ДД.ММ.ГГГГ (пример: 14.10.2018)"""
+
     date_formate = date_['date']
     the_date = datetime.datetime.strptime(date_formate, '%Y-%m-%dT%H:%M:%S.%f')
     th_date = the_date.strftime('%d.%m.%Y')
     return th_date
 
 
-def operation_from(operation):
-    """"""
+def operation_from_and_to(operation):
+    """Маскирование карты или счёта"""
     operation_from = operation.get('from')
     operation_to = operation.get('to')
 
--- main.py
+++
@@ -1,5 +0,0 @@
-# file_path = '../data/operations.json'
-# json_data = open_json_file(file_path)
-# a = last_executed_five(json_data)[:5]
-# for i in a:
-#     print(operation_from(i))
